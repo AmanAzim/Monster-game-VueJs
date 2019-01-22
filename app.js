bb9667fad52ec9b51339aa10be8c370ecbc8dbc7
Aman <rumman.djuice1@gmail.com>
2019-01-22 20:45:33 +0100

    monster commit

--- app.js
+++ app.js
@@ -12,29 +12,46 @@ new Vue({
             this.playerHealth=100;
         },
         attack:function () {
-            var max=10;
-            var min=3;
-            var damage=Math.max(Math.floor(Math.random()*max) + 1, min);
+
+            var damage=this.calculateDamage(10, 3);
             this.monsterHealth -=damage;
 
-            if(this.monsterHealth<=0)
+            if(this.checkWin())
             {
-                alert('You won!');
-                this.gameIsRunning=false;
                 return;
             }
 
-            max=12;
-            min=5;
-            var damage=Math.max(Math.floor(Math.random()*max) + 1, min);
+            var damage=this.calculateDamage(12, 5);
             this.playerHealth -=damage;
 
-            if(this.playerHealth<=0)
-            {
-                alert('You lost :(');
-                this.gameIsRunning=false;
-            }
+            this.checkWin();
 
+        },
+        calculateDamage:function(max, min){
+            return Math.max(Math.floor(Math.random()*max) + 1, min);
+        },
+        checkWin:function(){
+            if(this.monsterHealth<=0)
+            {
+                if(confirm('You won, want to start a new game ?'))
+                {
+                    this.startGame();
+                }else{
+                    this.gameIsRunning=false;
+                }
+                return true;
+            }
+            else if(this.playerHealth<=0)
+            {
+                if(confirm('You lost :( want to start a new game ?'))
+                {
+                    this.startGame();
+                }else{
+                    this.gameIsRunning=false;
+                }
+                return true;
+            }
+            return false;
         },
         specialAttack:function () {
             
